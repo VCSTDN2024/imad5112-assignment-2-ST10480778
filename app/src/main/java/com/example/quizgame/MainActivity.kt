fd532c4c26d90b9237ee0a8dd52c405c5245fcfb
Yenziwe-12 <146882693+Yenziwe-12@users.noreply.github.com>
2025-04-09 12:51:15 +0200

    first commit blank

---
+++ app/src/main/java/com/example/quizgame/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.quizgame
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var  startBtn: Button
+    private lateinit var exitBtn : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        startBtn = findViewById(R.id.startBtn)
+        exitBtn=findViewById(R.id.exitBtn)
+
+
+        startBtn.setOnClickListener {
+
+
+        }
+
+
+        exitBtn.setOnClickListener {
+
+            finishAffinity()
+
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
