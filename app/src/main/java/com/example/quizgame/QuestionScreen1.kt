adc7fc1fec3226687f38cc5fd019453bd05fc2ab
Yenziwe-12 <146882693+Yenziwe-12@users.noreply.github.com>
2025-04-20 16:34:20 +0200

    fixed my typo for question screen

--- app/src/main/java/com/example/quizgame/QuestionScreen1.kt
+++ app/src/main/java/com/example/quizgame/QuestionScreen1.kt
@@ -1,4 +1,5 @@
 package com.example.quizgame
+
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -6,19 +7,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class questionscreen : AppCompatActivity() {
-    private lateinit var posBtn:Button
-    private lateinit var negBtn:Button
-    private lateinit var nextBtn:Button
+class QuestionScreen1 : AppCompatActivity() {
+    private lateinit var posBtn: Button
+    private lateinit var negBtn: Button
+    private lateinit var nextBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_questionscreen)
-
+        setContentView(R.layout.activity_question_screen1)
+        
         posBtn=findViewById(R.id.posBtn)
         negBtn=findViewById(R.id.negBtn)
         nextBtn=findViewById(R.id.nextBtn)
+        val category= arrayOf(
+            "General Knowledge"
+        )
         val questions= arrayOf(
             "It takes a sloth two weeks to digest a meal?",
             "An ant can lift 1000 times its body weight?",
@@ -32,13 +36,14 @@ class questionscreen : AppCompatActivity() {
         posBtn.setOnClickListener {
 
         }
-         negBtn.setOnClickListener {
+        negBtn.setOnClickListener {
 
-         }
+        }
 
         nextBtn.setOnClickListener {
 
         }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
