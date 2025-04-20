7fdb3a40239ef3b12d1f8f33058292a13fab90ed
Yenziwe-12 <146882693+Yenziwe-12@users.noreply.github.com>
2025-04-20 15:57:38 +0200

    added a parallel array

--- app/src/main/java/com/example/quizgame/questionscreen.kt
+++ app/src/main/java/com/example/quizgame/questionscreen.kt
@@ -1,16 +1,44 @@
 package com.example.quizgame
-
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class questionscreen : AppCompatActivity() {
+    private lateinit var posBtn:Button
+    private lateinit var negBtn:Button
+    private lateinit var nextBtn:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_questionscreen)
+
+        posBtn=findViewById(R.id.posBtn)
+        negBtn=findViewById(R.id.negBtn)
+        nextBtn=findViewById(R.id.nextBtn)
+        val questions= arrayOf(
+            "It takes a sloth two weeks to digest a meal?",
+            "An ant can lift 1000 times its body weight?",
+            "The goat is the natural animal of Scotland?",
+            "South Africa has three capital?",
+            "Mount Everest is the tallest mountain in the world?"
+        )
+        val answers = booleanArrayOf(true,false,false,true,true)
+
+
+        posBtn.setOnClickListener {
+
+        }
+         negBtn.setOnClickListener {
+
+         }
+
+        nextBtn.setOnClickListener {
+
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
